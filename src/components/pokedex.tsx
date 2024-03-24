0e68681d4c790b061feed0be89ad568a5143a34d
Iván López Ávila <149825835+ivanloav@users.noreply.github.com>
2024-03-24 19:30:07 +0100

    Actividad finalizada

--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -7,6 +7,7 @@ import { Button } from "./button";
 import { LedDisplay } from "./led-display";
 
 import { weaknesses, typeTranslations } from "../@types/pokemon-weakness-translation";
+import type { Pokemon } from "models";
 
 import "./pokedex.css";
 
@@ -17,6 +18,9 @@ export function Pokedex() {
   const [i, setI] = useState(0);
   const { pokemon: selectedPokemon } = usePokemon(pokemonList[i]);
   const { pokemon: nextPokemon } = usePokemon(pokemonList[i + 1]);
+
+  const [selectedTeam, setSelectedTeam] = useState<Pokemon[]>([]);
+
 
   const prev = () => {
     resetTransition();
@@ -34,7 +38,7 @@ export function Pokedex() {
     setI((i) => i + 1);
   };
 /* **************************************************************************************** */
-  const BASE_SRC_IMG = "/src/assets/types/";
+  const BASE_SRC_IMG = "/src/assets/img/";
   
   const getCombinedWeaknesses = (types: string[]) => {
     const allWeaknesses: string[] = [];
@@ -57,81 +61,110 @@ export function Pokedex() {
 
   // Obtener las debilidades combinadas para los tipos del Pokémon
   const pokemonWeaknesses = getCombinedWeaknesses(selectedPokemon?.types.map(({ type }) => type.name) || []);
+  
+  // Función para manejar la adición de Pokémon al equipo
+  const handleAddToTeam = () => {
+    if (selectedPokemon && selectedTeam.length < 6 && !selectedTeam.find(p => p.id === selectedPokemon.id)) {
+      setSelectedTeam(prev => [...prev, selectedPokemon]);
+    } else {
+      alert("Tu equipo ya está completo o el Pokémon ya está en el equipo.");
+    }
+  };
 /* **************************************************************************************** */
-  return (
-    <div className={c("pokedex", `pokedex-${theme}`)}>
-      <div className="pokeWeakness">
-          <h3>Debilidades</h3>
-          <div className="weaknessesImg-container">
-            {pokemonWeaknesses.map((weakness, index) => (
-              <div className="weaknessesImg" key={index}>
-                <img src={BASE_SRC_IMG + weakness + ".webp"} alt={weakness} />
-                <p className={c({ 'text-black': theme === 'yellow' })}>{translateType(weakness)}</p>
-              </div>
-            ))}
-          </div>
-      </div>
-      <div className="panel left-panel">
-        <div className="screen main-screen">
-          {selectedPokemon && (
-            <img
-              className={c(
-                "sprite",
-                "obfuscated",
-                ready && "ready",
-                ready && `ready--${randomMode()}`
-              )}
-              src={selectedPokemon.sprites.front_default}
-              alt={selectedPokemon.name}
-            />
-          )}
+return (
+  <div className={c("pokedex", `pokedex-${theme}`)}>
+    <div className="pokeWeakness">
+        <h3>Debilidades</h3>
+        <div className="weaknessesImg-container">
+          {pokemonWeaknesses.map((weakness, index) => (
+            <div className="weaknessesImg" key={index}>
+              <img src={BASE_SRC_IMG + weakness + ".webp"} alt={weakness} />
+              <p className={c({ 'text-black': theme === 'yellow' })}>{translateType(weakness)}</p>
+            </div>
+          ))}
         </div>
-        <div className="screen name-display">
-          <div
+    </div>
+    <div className="panel left-panel">
+      <div className="screen main-screen">
+        {selectedPokemon && (
+          <img
             className={c(
-              "name",
+              "sprite",
               "obfuscated",
               ready && "ready",
               ready && `ready--${randomMode()}`
             )}
-          >
-            {selectedPokemon?.name}
-          </div>
-        </div>
-        <div className="pokeType">
-          {selectedPokemon?.types.map((typeEntry, index) => (
-            <div key={index}>
-              <img src={BASE_SRC_IMG + typeEntry.type.name + ".webp"} alt={typeEntry.type.name} />
-              <p className={c({ 'text-black': theme === 'yellow' })}>{translateType(typeEntry.type.name)}</p>
-            </div>
-          ))}
+            src={selectedPokemon.sprites.front_default}
+            alt={selectedPokemon.name}
+          />
+        )}
+      </div>
+      <div className="screen name-display">
+        <div
+          className={c(
+            "name",
+            "obfuscated",
+            ready && "ready",
+            ready && `ready--${randomMode()}`
+          )}
+        >
+          {selectedPokemon?.name}
         </div>
       </div>
-      <div className="panel right-panel">
-        <div className="controls leds">
-          <LedDisplay color="blue" />
-          <LedDisplay color="red" />
-          <LedDisplay color="yellow" />
-        </div>
-        <div className="screen second-screen">
-          {nextPokemon && (
-            <img
-              className={c(
-                "sprite",
-                "obfuscated",
-                ready && "ready",
-                ready && `ready--${randomMode()}`
-              )}
-              src={nextPokemon.sprites.front_default}
-              alt={nextPokemon.name}
-            />
-          )}
-        </div>
-        <div className="controls">
-          <Button label="prev" onClick={prev} />
-          <Button label="next" onClick={next} />
-        </div>
+      <div className="pokeType">
+        {selectedPokemon?.types.map((typeEntry, index) => (
+          <div key={index}>
+            <img src={BASE_SRC_IMG + typeEntry.type.name + ".webp"} alt={typeEntry.type.name} />
+            <p className={c({ 'text-black': theme === 'yellow' })}>{translateType(typeEntry.type.name)}</p>
+          </div>
+        ))}
       </div>
     </div>
-  );
+    <div className="panel right-panel">
+      <div className="controls leds">
+        <LedDisplay color="blue" />
+        <LedDisplay color="red" />
+        <LedDisplay color="yellow" />
+      </div>
+      <div className="screen second-screen">
+        {nextPokemon && (
+          <img
+            className={c(
+              "sprite",
+              "obfuscated",
+              ready && "ready",
+              ready && `ready--${randomMode()}`
+            )}
+            src={nextPokemon.sprites.front_default}
+            alt={nextPokemon.name}
+          />
+        )}
+      </div>
+      
+      <div className="teamButton">
+        <img src={BASE_SRC_IMG + "PokedexButton.png"} onClick={handleAddToTeam} alt="Añadir al Equipo" />
+        <p className={c({ 'text-black': theme === 'yellow' })}>Añadir al Equipo</p>
+      </div>
+
+      <div className="controls">
+        <Button label="prev" onClick={prev} />
+        <Button label="next" onClick={next} />
+      </div>
+    </div>
+
+    <div className="teamSelected">
+      <h3>Equipo Seleccionado:</h3>
+      {selectedTeam.map((pokemon) => (
+        <div key={pokemon.name} className="teamSelectedList">
+          <p className={c({ 'text-black': theme === 'yellow' })}>{pokemon.name.toUpperCase()}</p>
+          <img
+            className={c("sprite", "obfuscated", ready && "ready", ready && `ready--${randomMode()}`)}
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+          />
+        </div>
+      ))}
+    </div>
+  </div>
+);
 }
